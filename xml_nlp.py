3f82413829ed256e5e9b3a663113f82fd054efc8
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-08-18 09:16:10 +0100

    Write out count and tf-idf to csv

--- xml_nlp.py
+++ xml_nlp.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import string
 from lxml import etree
 import nltk
@@ -7,12 +8,13 @@ from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 nltk.download("all", quiet=True)
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 preprocessing = "lemmatize"  # "stem"
 
 with open("MsgLog_2017-01-24-011347.xml", "rt") as f:
     s = f.read()
+
 parser = etree.XMLParser(recover=True)
 tree = etree.fromstring(s, parser=parser)
 
@@ -22,10 +24,6 @@ stop_words = set(stopwords.words('english'))
 ps = PorterStemmer()
 lt = WordNetLemmatizer()
 
-s = s.translate(str.maketrans('','',string.punctuation))
-s = s.translate(str.maketrans('','',string.digits))
-s = s.lower()
-
 for tag in ["Subject", "Body"]:
     texts = []
     for node in tree.iter(tag=tag):
@@ -33,6 +31,8 @@ for tag in ["Subject", "Body"]:
         content = node.text.translate(str.maketrans('','',string.punctuation))
         # remove digits
         content = content.translate(str.maketrans('','',string.digits))
+        # lowercase
+        content = content.lower()
         # tokenize
         word_list = word_tokenize(content)
         # remove stop words and words not in dictionary
@@ -46,15 +46,18 @@ for tag in ["Subject", "Body"]:
     
         texts.append(word_list)
 
-    text_strings = [" ".join(words) for words in texts] 
-    text_string = [" ".join(ts) for ts in text_strings] 
-
+    text_strings = [" ".join(words) for words in texts]
+    text_string = " ".join(text_strings)
+  
     vect = TfidfVectorizer()
-    tfidf = vect.fit_transform([text_string])
+    tfidf = vect.fit_transform([text_string]).toarray().flatten()
     tfidf_terms = vect.get_feature_names()
-    tfidf_words = pd.Series(tfidf.toarray().flatten(),
-                            index=tfidf_terms)
-    tfidf_words.to_csv("TF_{}.csv".format(tag))
 
-
-
+    vect = CountVectorizer()
+    count = vect.fit_transform([text_string]).toarray().flatten()
+    count_terms = vect.get_feature_names()
+    
+    output = pd.DataFrame({"term": count_terms, "occurence": count})
+    output.index = output["term"]
+    output["TF-IDF"] = tfidf
+    output[["occurence", "TF-IDF"]].to_csv("TF_{}.csv".format(tag))
